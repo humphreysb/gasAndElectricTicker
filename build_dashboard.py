60e5a87e67c59a03227b6b5cced6fd3630a6a812
Brad Humphreys <humphreysbt@gmail.com>
2026-03-11 19:33:23 -0400

    Fixes to scraper and added multiple regions

--- build_dashboard.py
+++ build_dashboard.py
@@ -1,22 +1,36 @@
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 df = pd.read_csv("elec_gas_data.csv")
 
 df["Date"] = pd.to_datetime(df["Date"])
 
-df["Electric7"] = df["ElectricPrice"].rolling(7).mean()
-df["Gas7"] = df["GasPrice"].rolling(7).mean()
+#df["Electric7"] = df["ElectricPrice"].rolling(7).mean()
+#df["Gas7"] = df["GasPrice"].rolling(7).mean()
 
-fig = go.Figure()
+
+fig = make_subplots(
+    rows=2, cols=1,
+    subplot_titles=("Electric Rate", "Gas Rate"))
+
+
+#fig = go.Figure()
+'''
+        fig.add_trace(go.Scatterpolar(
+            r=currentPolar["BTV"], theta=currentPolar["AWA"], mode='markers+lines', line=dict(color=color, width=2, shape='spline'),
+            name=f"{tw} kts TWS", legendgroup=f"tws{tw}", showlegend=False
+        ), row=1, col=1)
+'''
 
 fig.add_trace(
 go.Scatter(
 x=df["Date"],
-y=df["ElectricPrice"],
+y=df["ElecPrice"],
 name="Electric ($/kWh)",
-mode="lines+markers"
-)
+mode="lines+markers",
+showlegend=False
+), row=1, col=1
 )
 
 fig.add_trace(
@@ -24,33 +38,21 @@ go.Scatter(
 x=df["Date"],
 y=df["GasPrice"],
 name="Gas ($/Mcf)",
-mode="lines+markers"
-)
-)
-
-fig.add_trace(
-go.Scatter(
-x=df["Date"],
-y=df["Electric7"],
-name="Electric 7-day avg",
-line=dict(dash="dash")
-)
+mode="lines+markers",
+showlegend=False
+), row=2, col=1
 )
 
-fig.add_trace(
-go.Scatter(
-x=df["Date"],
-y=df["Gas7"],
-name="Gas 7-day avg",
-line=dict(dash="dash")
-)
-)
 
+
+
+''''
 fig.update_layout(
 title="Ohio Energy Price Tracker",
 xaxis_title="Date",
 yaxis_title="Price",
 template="plotly_white"
 )
+'''
 
 fig.write_html("index.html")
--- energy_scraper.py
+++ energy_scraper.py
@@ -4,6 +4,7 @@ import urllib3
 from datetime import date
 import csv
 import os
+from io import StringIO
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -19,7 +20,10 @@ def get_min_offer(params):
         verify=False
     )
 
-    tables = pd.read_html(response.text)
+    response.raise_for_status()
+
+    html_wrapped = StringIO(response.text)
+    tables = pd.read_html(html_wrapped)
     df = tables[0]
 
     df.columns = df.columns.str.strip()
@@ -34,34 +38,91 @@ def get_min_offer(params):
     df = df[df['Monthly Fee'].str.contains(r'\$0', regex=True)]
     df = df[df['promo. offers'].str.contains('No', case=False)]
 
-    key = '$/kWh' if params["Category"] == "Electric" else '$/Mcf'
+    
+    a=1
+    if params["Category"] == "Electric":
+        key = '$/kWh' 
+    else:
+        if '$/Mcf' in df.columns:
+            key = '$/Mcf'
+            a=10
+        else:
+            key = '$/Ccf'
 
-    df[key] = pd.to_numeric(df[key])
+
+
+    df[key] = pd.to_numeric(df[key])/a
 
     row = df.loc[df[key].idxmin()]
 
     return row["Supplier"], row[key]
 
 
-today = date.today()
 
-elec_params = {
-"Category": "Electric",
-"TerritoryId": 6,
-"RateCode": 1
-}
+elecProviders = {9:'AES Power',
+    2:'AEP',
+    4:'Duke',
+    7:'Ohio Edison',
+    6:'Ilumminating Co',
+    3:'Toledo Edison'}
 
-gas_params = {
-"Category": "NaturalGas",
-"TerritoryId": 1,
-"RateCode": 1
-}
-
-elec_supplier, elec_price = get_min_offer(elec_params)
-gas_supplier, gas_price = get_min_offer(gas_params)
+gasProviders = {1:'Enbridge-Dominion',
+                11:'Centerpoint',
+                10:'Duke',
+                8:'Columbia'}
 
 file = "elec_gas_data.csv"
 
+today = date.today()
+
+data_list = []
+
+
+for key in elecProviders:
+    params = {
+    "Category": "Electric",
+    "TerritoryId": key,
+    "RateCode": 1
+    }
+
+    supplier, price = get_min_offer(params)
+
+    new_data = {"Date": today, 
+                "ElecOrGas": 'elec', 
+                'TerritoryId': key, 
+                'Rate':price, 
+                'Supplier':supplier}
+    data_list.append(new_data)
+    
+
+for key in gasProviders:
+    params = {
+    "Category": "Gas",
+    "TerritoryId": key,
+    "RateCode": 1
+    }
+
+    supplier, price = get_min_offer(params)
+
+    new_data = {"Date": today, 
+                "ElecOrGas": 'gas', 
+                'TerritoryId': key, 
+                'Rate':price, 
+                'Supplier':supplier}
+    data_list.append(new_data)
+
+
+df = pd.DataFrame(data_list)
+dfFile = pd.read_csv(file)
+df = pd.concat([dfFile, df])
+
+
+
+df.to_csv(file, index=False) 
+
+
+
+'''
 if not os.path.exists(file):
     with open(file, "w", newline="") as f:
         writer = csv.writer(f)
@@ -70,3 +131,4 @@ if not os.path.exists(file):
 with open(file, "a", newline="") as f:
     writer = csv.writer(f)
     writer.writerow([today, elec_price, elec_supplier, gas_price, gas_supplier])
+'''
